48ea59bbdd21c69595f030ddc6fee9cd24b26390
mMockyyy <mmlconcepcion@tip.edu.ph>
2026-04-03 14:51:39 +0800

    Add global error handlers to catch unhandled rejections

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,17 @@ import { app } from "./app";
 import { env } from "./config/env";
 import { connectDb } from "./config/db";
 
+// Global error handlers
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+  process.exit(1);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+  process.exit(1);
+});
+
 async function bootstrap() {
   await connectDb(env.MONGODB_URI);
   app.listen(env.PORT, () => {
